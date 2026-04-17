0b02ca9fe6fa70ca33965ef0f22848d1476ead5a
Doraemon cyber team <117832562+Baba01hacker666@users.noreply.github.com>
2026-04-17 12:07:22 +0545

    Format __all__ for better readability

--- server/capture/__init__.py
+++ server/capture/__init__.py
@@ -3,4 +3,9 @@ from .x11 import X11Capture
 from .framebuffer import FramebufferCapture
 from .wayland import WaylandCapture
 
-__all__ = ["CaptureBackend", "X11Capture", "FramebufferCapture", "WaylandCapture"]
+__all__ = [
+    "CaptureBackend",
+    "X11Capture",
+    "FramebufferCapture",
+    "WaylandCapture",
+]
