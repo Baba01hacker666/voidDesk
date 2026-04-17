ccab6ef4ec9514cf24ce943e8a3e40a3249810f7
Doraemon cyber team <117832562+Baba01hacker666@users.noreply.github.com>
2026-04-17 12:20:12 +0545

    Update x11.py

--- server/capture/x11.py
+++ server/capture/x11.py
@@ -1,13 +1,16 @@
 """
 X11 capture backend using x11grab
 """
+
 import os
 import shutil
 from .base import CaptureBackend
 
 
 class X11Capture(CaptureBackend):
-    def __init__(self, display: str = ":0", resolution: str = "1280x720", fps: int = 30):
+    def __init__(
+        self, display: str = ":0", resolution: str = "1280x720", fps: int = 30
+    ):
         self.display = display
         self.resolution = resolution
         self.fps = fps
@@ -19,9 +22,14 @@ class X11Capture(CaptureBackend):
 
     def get_ffmpeg_input_args(self) -> list:
         return [
-            "-f", "x11grab",
-            "-framerate", str(self.fps),
-            "-video_size", self.resolution,
-            "-draw_mouse", "1",
-            "-i", self.display,
+            "-f",
+            "x11grab",
+            "-framerate",
+            str(self.fps),
+            "-video_size",
+            self.resolution,
+            "-draw_mouse",
+            "1",
+            "-i",
+            self.display,
         ]
